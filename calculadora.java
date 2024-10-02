dae7113a395e84613e9a286e1e996129e8fe7c7f
Nicolayyy <niclay.baquero.2023@upb.edu.co>
2024-10-02 13:43:21 -0500

    primer commit

--- calculadora.java
+++ calculadora.java
@@ -1,1 +1,35 @@
+package calculadora; 
+
+Public class operaciones {
+
+    private double num1;
+    private double num2;
+
+    public operaciones ( double num1, double num2) {
+        this.num1 = num1;
+        this.num2 = num2;
+        } 
+
+        public double suma () {
+            return num1 + num2;
+            } 
+
+            public double resta () {
+            return num1 - num2;
+            } 
+
+            public double multiplicacion () {
+            return num1 * num2;
+            } 
+
+            public double division () {
+            if (num2 != 0 ) {
+                return num1 / num2;
+                } else { 
+                    system.out.println ("no se puede dividir entre cero");
+
+
+
+
+
 
